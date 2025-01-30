2bee9253ae5f0ac035d7e6a302c4a2d140abb4cc
Jean-Luc Liziki <jl24aaj@herts.ac.uk>
2025-01-30 19:51:27 +0000

    Setting up the basics: game loop, background images/title

--- Game_Word_py/Game_Word_py.py
+++ Game_Word_py/Game_Word_py.py
@@ -1,21 +1,30 @@
 import pygame
+import os
 
 pygame.init()
 
+#Screen initialization...
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Word Game")
 
-background = pygame.Rect((0, 0, 1280, 720))
+#Load background image from local path/rescale
+background_image = pygame.transform.scale(pygame.image.load(os.path.join('Background.png')), 
+                                                             (SCREEN_WIDTH, SCREEN_HEIGHT))
+#Load title image from local path/rescale
+title_image = pygame.transform.scale(pygame.image.load(os.path.join('Title.png')), 
+                                                                       (473, 70))
 
 #Game loop.
 game_running = True
 while game_running:
 
-    pygame.draw.rect(game_screen, (255,255, 255), background) #Draw a white background
+    pygame.Surface.blit(game_screen, background_image, (0, 0))
+    pygame.Surface.blit(game_screen, title_image, (640 - (473 / 2), 0))
 
-    for event in pygame.event.get():
+    for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             game_running = False
 
