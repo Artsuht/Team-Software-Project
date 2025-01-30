ae1a4429449cd390e5ff8feed647e9188fb68b2d
Jean-Luc Liziki <jl24aaj@herts.ac.uk>
2025-01-30 20:07:38 +0000

    Implemented dictionary.

--- Game_Word_py/Game_Word_py.py
+++ Game_Word_py/Game_Word_py.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import pygame
 import os
 
@@ -11,18 +12,33 @@ game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Word Game")
 
 #Load background image from local path/rescale
-background_image = pygame.transform.scale(pygame.image.load(os.path.join('Background.png')), 
+game_background_image = pygame.transform.scale(pygame.image.load(os.path.join('Background.png')), 
                                                              (SCREEN_WIDTH, SCREEN_HEIGHT))
-#Load title image from local path/rescale
 title_image = pygame.transform.scale(pygame.image.load(os.path.join('Title.png')), 
                                                                        (473, 70))
+main_menu_background = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
-#Game loop.
+
+#Fill dictionary with words from the data set.
+dictionary = []
+with open('Word Set.txt', 'r') as word_set:
+ for word in word_set:
+    dictionary.append(word.strip())
+
+#Menu
+in_menu = True
+
+#Game loop
 game_running = True
 while game_running:
 
-    pygame.Surface.blit(game_screen, background_image, (0, 0))
-    pygame.Surface.blit(game_screen, title_image, (640 - (473 / 2), 0))
+    #Main Menu
+    if in_menu:
+         pygame.draw.rect(game_screen, (255, 255, 255), main_menu_background)
+         pygame.Surface.blit(game_screen, title_image, (640 - (473 / 2), 0))
+    else:
+        pygame.Surface.blit(game_screen, game_background_image, (0, 0))
+        pygame.Surface.blit(game_screen, title_image, (640 - (473 / 2), 0))
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
